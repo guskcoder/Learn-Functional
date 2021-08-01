34b14315d18b847a0b12f9c9199c381cadd0d25e
guskcoder <gustavo@imaginative.company>
2021-08-01 15:31:03 -0300

    Recursion Shop Add

---
+++ recursion/enchanter_shop.ex
@@ -0,0 +1,8 @@
+def test_data do
+  [
+    %{title: "Longsword", price: 50, magic: false}
+    %{title: "Healing Potion", price: 60, magic: true}
+    %{title: "Rope", price: 10, magic: false}
+    %{title: "Dragon's Spear", price: 100, magic: true}
+  ]
+end
