1e1ee93266a92f5e73092c31ed0d73531e7f6ac4
guskcoder <gustavo@imaginative.company>
2021-08-03 20:16:10 -0300

    Recursion Add New Fucntion

--- recursion/enchanter_shop.ex
+++ recursion/enchanter_shop.ex
@@ -6,3 +6,17 @@ def test_data do
     %{title: "Dragon's Spear", price: 100, magic: true}
   ]
 end
+
+@enchanter_name "Edwin"
+
+def enchant_for_sale([]), do: []
+
+def enchant_for_sale([item | incoming_items]) do
+  new_item = %{
+    title: "#{@enchanter_name}'s #{item.title}",
+    price: item.price * 3,
+    magic: true
+  }
+
+  [new_item | enchant_for_sale(incoming_items)]
+end
