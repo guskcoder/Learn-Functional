5e7cecb75efccce553afe96e19338469305e60de
Gustavo Macbook <gustavo@imaginative.company>
2021-08-30 21:19:18 -0300

    Game Add

---
+++ recursion/lib/enchanter_shop.ex
@@ -0,0 +1,22 @@
+def test_data do
+  [
+    %{title: "Longsword", price: 50, magic: false},
+    %{title: "Healing Potion", price: 60, magic: true},
+    %{title: "Rope", price: 10, magic: false},
+    %{title: "Dragon's Spear", price: 100, magic: true}
+  ]
+end
+
+@enchanter_name "Edwin"
+
+def enchant_for_sale([]), do: []
+
+def enchant_for_sale([item | incoming_items]) do
+  new_itens = %{
+    title: "#{@enchanter_name}'s #{item.title}",
+    price: item.price * 3,
+    magic: true
+  }
+
+  [new_item | enchant_for_sale(incoming_items)]
+end
